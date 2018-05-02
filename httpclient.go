3bcb9778c7ece5d80111f20f3ed6ad45e955ae6f
ssOlexBaiko <obaiko@softserveinc.com>
2018-05-02 15:36:10 +0300

    review of 1st home assignment

--- httpclient.go
+++ httpclient.go
@@ -1,28 +1,34 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-	"io/ioutil"
 	"encoding/json"
-	"os"
 	"flag"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
 )
 
+// заховай ключ через аргументи прапорця
+// https://golang.org/doc/effective_go.html#mixed-caps
 const api_key = "WsJyksJKHPWzjL7dWwhRGx8PXp6VJkZV"
-func main()  {
-	var gifs_limit = flag.Int("gifs_limit", 5, "gifs limit number")
+
+// виносим змінну в heap
+var gifs_limit = flag.Int("gifs_limit", 5, "gifs limit number")
+
+type Data []struct {
+	Type  string `json:"type""`
+	Id    string `json:"id"`
+	Title string `json:"title"`
+	Url   string `json:"url"`
+}
+type Trenging struct {
+	Data Data `json:"data"`
+}
+
+func main() {
 	flag.Parse()
-	type Data []struct {
-		Type string `json:"type""`
-		Id string `json:"id"`
-		Title string `json:"title"`
-		Url string `json:"url"`
-	}
-	type Trenging struct{
-		Data Data `json:"data"`
-	}
-	var api_key = "WsJyksJKHPWzjL7dWwhRGx8PXp6VJkZV"
+
 	var url = fmt.Sprintf("http://api.giphy.com/v1/gifs/trending?api_key=%s&limit=%d", api_key, *gifs_limit)
 	resp, error := http.Get(url)
 	if error != nil {
@@ -30,23 +36,26 @@ func main()  {
 		return
 	}
 	defer resp.Body.Close()
+
 	contents, error := ioutil.ReadAll(resp.Body)
-	if error!= nil {
+	if error != nil {
 		fmt.Printf("Error during reading response: %s", error)
 		return
 	}
 	file, error := os.Create("data")
-	if error!= nil {
+	if error != nil {
 		fmt.Printf("Error during file creation: %s", error)
 		return
 	}
 	defer file.Close()
+
+	// https://golang.org/doc/effective_go.html#mixed-caps
 	bytes_written, error := file.Write(contents)
-	if error!= nil {
+	if error != nil {
 		fmt.Printf("Error during file update: %s", error)
 		return
 	}
-	fmt.Printf("wrote %d bytes to file\n",bytes_written)
+	fmt.Printf("wrote %d bytes to file\n", bytes_written)
 
 	var gifs Trenging
 	err := json.Unmarshal(contents, &gifs)
@@ -55,7 +64,7 @@ func main()  {
 		return
 	}
 
-	for _, gif := range(gifs.Data){
+	for _, gif := range gifs.Data {
 		fmt.Printf("Gif Name: %s, Gif URL: %s\n", gif.Title, gif.Url)
 	}
 }
